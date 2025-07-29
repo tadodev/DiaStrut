73030caceebf530e64feaa39b80b51e10a2d6ca6
Saitoh Chido <dothanhtupt@gmail.com>
2025-07-29 16:11:05 +0700

    Add project files.

---
+++ src/DiaStrut.Core/GeometryComponent.cs
@@ -0,0 +1,34 @@
+﻿using Rhino.Geometry;
+
+namespace DiaStrut.Core;
+
+public class GeometryComponent
+{
+    public GeometryComponent()
+    {
+        // Constructor logic here
+    }
+
+    public static Curve CreateSpiral(Plane plane, double r0, double r1, int turns) // CA1822: Marked as static
+    {
+        var l0 = new Line(plane.Origin + r0 * plane.XAxis, plane.Origin + r1 * plane.XAxis); // IDE0090: Simplified 'new' expression
+        var l1 = new Line(plane.Origin - r0 * plane.XAxis, plane.Origin - r1 * plane.XAxis); // IDE0090: Simplified 'new' expression
+
+        l0.ToNurbsCurve().DivideByCount(turns, true, out Point3d[] p0); // IDE0018: Variable declaration inlined
+        l1.ToNurbsCurve().DivideByCount(turns, true, out Point3d[] p1); // IDE0018: Variable declaration inlined
+
+        var spiral = new PolyCurve(); // IDE0090: Simplified 'new' expression
+
+        for (int i = 0; i < p0.Length - 1; i++)
+        {
+            var arc0 = new Arc(p0[i], plane.YAxis, p1[i + 1]); // IDE0090: Simplified 'new' expression
+            var arc1 = new Arc(p1[i + 1], -plane.YAxis, p0[i + 1]); // IDE0090: Simplified 'new' expression
+
+            spiral.Append(arc0);
+            spiral.Append(arc1);
+        }
+
+        return spiral;
+    }
+
+}
---
+++ src/DiaStrut.Plugin/DiaStrut.PluginInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace DiaStrut.Plugin
+{
+  public class DiaStrut_PluginInfo : GH_AssemblyInfo
+  {
+    public override string Name => "DiaStrut";
+
+    //Return a 24x24 pixel bitmap to represent this GHA library.
+    public override Bitmap Icon => null;
+
+    //Return a short string describing the purpose of this GHA library.
+    public override string Description => "";
+
+    public override Guid Id => new Guid("67e0c48e-aa62-4f25-bd57-299583125c3b");
+
+    //Return a string identifying you or your company.
+    public override string AuthorName => "";
+
+    //Return a string representing your preferred contact details.
+    public override string AuthorContact => "";
+
+    //Return a string representing the version.  This returns the same version as the assembly.
+    public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+  }
+}
